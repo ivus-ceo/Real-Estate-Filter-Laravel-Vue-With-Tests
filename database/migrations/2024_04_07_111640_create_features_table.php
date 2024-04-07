a5ed4eceec126f8aec735894d0d592f25bfa09db
i.uskov <i.uskov@whitewill.ru>
2024-04-07 11:18:22 +0300

    Filter, Store, Price and Areas modifications

--- database/migrations/2024_04_07_111640_create_features_table.php
+++ database/migrations/2024_04_07_111640_create_features_table.php
@@ -10,6 +10,7 @@ return new class extends Migration {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->morphs('featureable');
             $table->timestamps();
         });
     }
